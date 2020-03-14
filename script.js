f854ad11670e5b77a1e1f1074f190ee41b96730e
oran <orantal01@gmail.com>
2020-03-14 18:43:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const formSubmit = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#password2');
+
+formSubmit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+});
+
+const checkRequired = (inputs) => {
+    for (const input of inputs) {
+        if (input.value === '') {
+            showError(input, `${getFiledName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    }
+};
+
+const checkLength = (input, min, max) => {
+    const { value } = input;
+    if (value.length < min) {
+        showError(input, `${getFiledName(input)} must be at least ${min} characters`);
+    } else if (value.length > max) {
+        showError(input, `${getFiledName(input)} must be less than ${max} characters`);
+    }
+};
+
+const checkEmail = (input) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+};
+
+const checkPasswordsMatch = (password, password2) => {
+    const { value: value1 } = password;
+    const { value: value2 } = password2;
+    if (value1 !== value2) {
+        showError(password2, 'Passwords do not match');
+    }
+};
+
+const getFiledName = input => {
+    return input.name;
+};
+
+const showError = (input, message) => {
+    const formControl = input.parentElement;
+    formControl.setAttribute('class', 'form-control error');
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+};
+
+const showSuccess = input => {
+    const formControl = input.parentElement;
+    formControl.setAttribute('class', 'form-control success');
+};
